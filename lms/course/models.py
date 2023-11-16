245bf4a646075cf7000c2af86ce78a6b6f501066
tasim313 <mostasimmahmudtasim@gmail.com>
2023-11-16 19:37:11 +0600

    category model add str function

--- lms/course/models.py
+++ lms/course/models.py
@@ -15,6 +15,9 @@ class Category(models.Model):
     )
     name = models.CharField(max_length=100, db_index=True, blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 
 class Course(models.Model):
